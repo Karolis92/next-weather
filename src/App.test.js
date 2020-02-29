fd109b3767c8abd6045f8dc70f06bf667c3c9c9d
Karolis92 <19680461+Karolis92@users.noreply.github.com>
2020-02-29 13:26:34 +0200

    Recreate web app

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders learn react link', () => {
+  const { getByTestId } = render(<App />);
+  const linkElement = getByTestId('image');
+  expect(linkElement).toBeInTheDocument();
 });
