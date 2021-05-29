e1f7891362299d76f817b03807da0622e3d3c2c6
Karolis92 <Karolis92@users.noreply.github.com>
2021-05-29 12:49:41 +0300

    make it responsive

--- src/components/forecast/forecast.tsx
+++ src/components/forecast/forecast.tsx
@@ -1,11 +1,13 @@
+import classNames from "classnames";
 import { memo } from "react";
 import { OneCallWeatherDto } from "../../one-call-weather-dto.model";
 import ForecastBoxes from "../ForecastBoxes/ForecastBoxes";
 import ForecastHeader from "../ForecastHeader/ForecastHeader";
+import styles from "./Forecast.module.css";
 
 function Forecast({ forecast }: { forecast: OneCallWeatherDto }) {
   return (
-    <section className="section">
+    <section className={classNames("section", styles.wrapper)}>
       <ForecastHeader conditions={forecast.current} />
       <ForecastBoxes conditions={forecast.current} />
     </section>
